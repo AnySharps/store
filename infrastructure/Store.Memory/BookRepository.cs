d72453dcf5fcec35cdcbfebe631f94b869cad94b
misch.sania2017yandex.ru <misch.sania2017@yandex.ru>
2021-08-29 00:41:15 +0500

    Создание presentation

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using System.Linq;
 
 namespace Store.Memory
 {
-    public class BookRepository
+    public class BookRepository : IBookRepository
     {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language")
+        };
+
+        public Book[] GetByTitle(string titlePart)
+        {
+            return books
+                .Where(b => b.Title.Contains(titlePart))
+                .ToArray();
+        }
     }
 }
